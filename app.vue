95c547bc7964352b7e5cc7dcceb5a0d24c5cac63
karim boussakri <kboussakridev@gmail.com>
2024-05-15 17:17:56 +0200

    premier commit

---
+++ app.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <a aria-current="page" href="#" class="router-link-active router-link-exact-active">paxpar
+      <span class="inline-flex items-center font-medium rounded-md text-xs px-2 py-1 bg-primary-50 dark:bg-primary-400 dark:bg-opacity-10 text-primary-500 dark:text-primary-400 ring-1 ring-inset ring-primary-500 dark:ring-primary-400 ring-opacity-25 dark:ring-opacity-25 mb-0.5">.tech</span></a>
+      
+  </div>
+</template>
