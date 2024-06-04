dce4b107e7a9bbdde21559171d16804f64a47568
Philippe ENTZMANN <philippe.entzmann@gmail.com>
2024-06-04 09:54:05 +0200

    wip

--- back/README.md
+++
@@ -1,8 +0,0 @@
-# partir back du projet
-
-API endpoints en fastapi
-
-
-```
-poetry run fastapi dev main.py
-```
--- backend/app/main.py
+++ backend/app/main.py
@@ -3,6 +3,11 @@ from typing import Union
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from pypdf import PdfReader, PdfWriter
+from pypdf.annotations import FreeText
+import time
+from starlette.responses import FileResponse
+
 
 import tkinter as tk
 app = FastAPI()
@@ -48,6 +53,50 @@ def backend_function():
 @app.get("/api/data")
 def get_data():
     return {"message": "Hello from FastAPI"}
+
+
+@app.get("/invoice/{num}")
+def invoice_get(num: int):
+    """
+    Build an invoice from the given number.
+    Lorem ipsum ....
+    """
+    # build the PDF
+    # ...
+
+    # Fill the writer with the pages you want
+    # pdf_path = os.path.join(RESOURCE_ROOT, "crazyones.pdf")
+    # reader = PdfReader(pdf_path)
+    # page = reader.pages[0]
+    writer = PdfWriter()
+    # writer.add_page(page)
+    writer.add_blank_page(800.0, 800.0)
+
+    # Create the annotation and add it
+    annotation = FreeText(
+        text=f"Invoice #{num}, {time.time()} seconds",
+        rect=(50, 550, 200, 650),
+        font="Arial",
+        bold=True,
+        italic=True,
+        font_size="20pt",
+        font_color="00ff00",
+        border_color="ff0000",
+        background_color="cdcdcd",
+    )
+    writer.add_annotation(page_number=0, annotation=annotation)
+
+    # Write the annotated file to disk
+    with open("file.pdf", "wb") as fp:
+        writer.write(fp)
+
+    # return f"invoice {num}"
+    return FileResponse(
+        "file.pdf",
+        # media_type="application/octet-stream",
+        # filename=file_name,
+    )
+
 
 if __name__ == "__main__":
     import uvicorn
